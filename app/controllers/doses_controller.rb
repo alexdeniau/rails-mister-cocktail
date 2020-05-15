58dbe150a487c5775fecbea8be78517b3f7dbe5f
Alexandre Deniau <deniaualexandre@gmail.com>
2020-05-15 14:09:52 +0200

    e

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -20,7 +20,6 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
